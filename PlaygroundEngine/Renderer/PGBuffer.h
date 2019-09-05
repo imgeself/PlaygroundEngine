ce2c0c5d0078c96538411c7701027c46ec90747c
Huseyin Karakullukcu <karakullukcuhuseyin@gmail.com>
2019-09-05 22:49:07 +0300

    added base index and vertex buffer interfaces

---
+++ PlaygroundEngine/Renderer/PGBuffer.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "../Core.h"
+
+class PG_API IVertexBuffer {
+    virtual ~IVertexBuffer() = default;
+};
+
+class PG_API IIndexBuffer {
+    virtual ~IIndexBuffer() = default;
+};
+

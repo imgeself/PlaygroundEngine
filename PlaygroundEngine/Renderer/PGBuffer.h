55cb05d22cb6198dce9ffa8b584f0f6780042409
Huseyin Karakullukcu <karakullukcuhuseyin@gmail.com>
2019-09-05 23:03:49 +0300

    made destructors public in PGBuffer

--- PlaygroundEngine/Renderer/PGBuffer.h
+++ PlaygroundEngine/Renderer/PGBuffer.h
@@ -3,10 +3,12 @@
 #include "../Core.h"
 
 class PG_API IVertexBuffer {
+public:
     virtual ~IVertexBuffer() = default;
 };
 
 class PG_API IIndexBuffer {
+public:
     virtual ~IIndexBuffer() = default;
 };
 

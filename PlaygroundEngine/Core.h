afc0c6237e9b2c593dc3322888ea2c29a8e3d459
Huseyin Karakullukcu <karakullukcuhuseyin@gmail.com>
2019-09-05 23:04:24 +0300

    added new macros for dll

--- PlaygroundEngine/Core.h
+++ PlaygroundEngine/Core.h
@@ -5,3 +5,13 @@
 #include <assert.h>
 
 #define PG_ASSERT(x, string) { if(!(x)) { printf("Assertion Failed: %s\n", string); __debugbreak(); } }
+
+#define SAFE_RELEASE(p) { if(p) { p->Release(); } }
+
+#ifdef PLATFORM_WINDOWS
+    #ifdef _WINDLL
+        #define PG_API __declspec(dllexport)
+    #else
+        #define PG_API __declspec(dllimport)
+    #endif
+#endif

58d3a2b0efc5e77c0e71f1ee4142384334073754
Huseyin Karakullukcu <karakullukcuhuseyin@gmail.com>
2019-09-11 22:41:55 +0300

    implemented PG_API macro

--- PlaygroundEngine/Core.h
+++ PlaygroundEngine/Core.h
@@ -11,7 +11,11 @@
 #ifdef PLATFORM_WINDOWS
     #define DLL_EXPORT __declspec(dllexport)
     #define DLL_IMPORT __declspec(dllimport)
-    #define PG_API DLL_EXPORT
+    #ifdef _WINDLL
+        #define PG_API DLL_IMPORT
+    #else
+        #define PG_API DLL_EXPORT
+    #endif
 #endif
 
 struct SystemInitArguments {

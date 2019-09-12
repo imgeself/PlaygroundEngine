c6d15d378ec89ef78730db33b0e5dbacc606d132
Huseyin Karakullukcu <karakullukcuhuseyin@gmail.com>
2019-09-12 17:32:14 +0300

    delta time argument in the OnUpdate function

--- PlaygroundEngine/PGApplication.h
+++ PlaygroundEngine/PGApplication.h
@@ -7,7 +7,7 @@ public:
     virtual ~IApplication() = default;
 
     virtual void OnInit() = 0;
-    virtual void OnUpdate() = 0;
+    virtual void OnUpdate(float deltaTime) = 0;
     virtual void OnRender() = 0;
     virtual void OnUIRender() = 0;
     virtual void OnExit() = 0;

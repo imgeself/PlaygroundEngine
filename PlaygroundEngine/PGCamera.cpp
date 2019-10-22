a9f7c2d6b680c67293dc7613680b0edae4dfff26
Huseyin Karakullukcu <karakullukcuhuseyin@gmail.com>
2019-10-22 22:17:20 +0300

    used LookAtLH function in PGCamera's SetView function

--- PlaygroundEngine/PGCamera.cpp
+++ PlaygroundEngine/PGCamera.cpp
@@ -11,17 +11,7 @@ void PGCamera::SetFrustum(uint32_t imageWidth, uint32_t imageHeight, float nearP
 
 void PGCamera::SetView(Vector3 cameraPosition, Vector3 targetPoint) {
     // Left-handed look-at function
-    const Vector3 upVector(0.0f, 1.0f, 0.0f);
-    const Vector3 cameraZ = Normalize(targetPoint - cameraPosition);
-    const Vector3 cameraX = Normalize(CrossProduct(upVector, cameraZ));
-    const Vector3 cameraY = Normalize(CrossProduct(cameraZ, cameraX));
-    Matrix4 viewMatrix;
-    viewMatrix[0] = Vector4(cameraX, -DotProduct(cameraPosition, cameraX));
-    viewMatrix[1] = Vector4(cameraY, -DotProduct(cameraPosition, cameraY));
-    viewMatrix[2] = Vector4(cameraZ, -DotProduct(cameraPosition, cameraZ));
-    viewMatrix[3][3] = 1.0f;
-
-    m_ViewMatrix = viewMatrix;
+    m_ViewMatrix = LookAtLH(cameraPosition, targetPoint);
     m_Position = cameraPosition;
 }
 

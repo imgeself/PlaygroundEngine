6cff1376e909dba62b339d6faf0212bc94aeed31
Huseyin Karakullukcu <karakullukcuhuseyin@gmail.com>
2019-11-16 23:19:49 +0300

    added new definitions for cascade shadow mapping

--- PlaygroundEngine/Assets/Shaders/ShaderDefinitions.h
+++ PlaygroundEngine/Assets/Shaders/ShaderDefinitions.h
@@ -3,31 +3,36 @@
 
 #define PER_DRAW_CBUFFER_SLOT 0
 #define PER_FRAME_CBUFFER_SLOT 1
+#define PER_SHADOWGEN_CBUFFER_SLOT 2
+
+#define RENDERER_VARIABLES_CBUFFER_SLOT 7
 
 #define SHADOW_MAP_TEXTURE2D_SLOT 0
 
-#define POINT_BORDER_SAMPLER_STATE_SLOT 0
+#define SHADOW_SAMPLER_COMPARISON_STATE_SLOT 0
+#define POINT_CLAMP_SAMPLER_STATE_SLOT 1
+
+// Variable defines
+#define MAX_SHADOW_CASCADE_COUNT 5
 
 #ifdef _HLSL
 #define CBUFFER(name, slotNumber) cbuffer name : register(b ## slotNumber)
 #define TEXTURE2D(name, slotNumber) Texture2D name : register(t ## slotNumber)
+#define TEXTURE2DARRAY(name, slotNumber) Texture2DArray name : register(t ## slotNumber)
 #define SAMPLER_STATE(name, slotNumber) SamplerState name : register(s ## slotNumber)
+#define SAMPLER_COMPARISON_STATE(name, slotNumber) SamplerComparisonState name : register(s ## slotNumber)
 #define Matrix4 row_major matrix
 #define Vector4 float4
 #define Vector3 float3
+#define Vector2 float2
+#define uint32_t uint
 #else 
 #define CBUFFER(name, registerNumber) struct name
 #define TEXTURE2D(name, slotNumber)
+#define TEXTURE2DARRAY(name, slotNumber)
 #define SAMPLER_STATE(name, slotNumber)
+#define SAMPLER_COMPARISON_STATE(name, slotNumber)
 #include "../../Math/math_util.h"
-
-enum SystemConstantBufferSlots {
-    SystemConstantBufferSlot_PerDraw = PER_DRAW_CBUFFER_SLOT,
-    SystemConstantBufferSlot_PerFrame = PER_FRAME_CBUFFER_SLOT,
-
-    SystemConstantBufferSlot_Count
-};
-
 #endif
 
 ///////////// Constant buffers
@@ -41,13 +46,29 @@ CBUFFER(PerFrameGlobalConstantBuffer, PER_FRAME_CBUFFER_SLOT) { // SystemConstan
     Vector4 cameraPos;
     Vector4 lightPos;
     Matrix4 lightViewMatrix;
-    Matrix4 lightProjMatrix;
+    Matrix4 lightProjMatrix[MAX_SHADOW_CASCADE_COUNT];
+
+    Matrix4 g_InverseViewProjMatrix;
+    Vector4 g_ScreenDimensions;
+};
+
+CBUFFER(PerShadowGenConstantBuffer, PER_SHADOWGEN_CBUFFER_SLOT) {
+    uint32_t g_ShadowGenCascadeIndex;
+    Vector3 pad00000001;
+};
+
+CBUFFER(RendererVariablesConstantBuffer, RENDERER_VARIABLES_CBUFFER_SLOT) {
+    uint32_t g_ShadowCascadeCount;
+    uint32_t g_ShadowMapSize;
+    Vector2 pad0000002;
 };
 
 /////////// Textures
-TEXTURE2D(g_ShadowMapTexture, SHADOW_MAP_TEXTURE2D_SLOT);
+TEXTURE2DARRAY(g_ShadowMapTexture, SHADOW_MAP_TEXTURE2D_SLOT);
 
-SAMPLER_STATE(g_PointBorderSampler, POINT_BORDER_SAMPLER_STATE_SLOT);
+/////////// Samplers
+SAMPLER_COMPARISON_STATE(g_ShadowMapSampler, SHADOW_SAMPLER_COMPARISON_STATE_SLOT);
+SAMPLER_STATE(g_PointClampSampler, POINT_CLAMP_SAMPLER_STATE_SLOT);
 
 
 #endif

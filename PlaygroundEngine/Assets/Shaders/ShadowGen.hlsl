f8a37f94d237a00f53bb636471227774fa35b575
Huseyin Karakullukcu <karakullukcuhuseyin@gmail.com>
2019-11-16 23:27:34 +0300

    select cascade's proj matrix in ShadowGen shader

--- PlaygroundEngine/Assets/Shaders/ShadowGen.hlsl
+++ PlaygroundEngine/Assets/Shaders/ShadowGen.hlsl
@@ -5,7 +5,7 @@
 ///////////////////////////////////////////////////////////////////////////
 float4 VSMain(float3 pos : Position, float3 normal : Normal) : SV_Position
 {
-    return mul(lightProjMatrix, mul(lightViewMatrix, mul(modelMatrix, float4(pos, 1.0f))));
+    return mul(lightProjMatrix[g_ShadowGenCascadeIndex], mul(lightViewMatrix, mul(modelMatrix, float4(pos, 1.0f))));
 }
 
 ///////////////////////////////////////////////////////////////////////////

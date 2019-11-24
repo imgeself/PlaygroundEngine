16a4178fc54e4db97d450ebadf3c7440690c5eef
Huseyin Karakullukcu <karakullukcuhuseyin@gmail.com>
2019-11-25 00:24:35 +0300

    Added PBR texture members into Material struct

--- PlaygroundEngine/Renderer/Material.h
+++ PlaygroundEngine/Renderer/Material.h
@@ -2,10 +2,16 @@
 
 #include "../Core.h"
 #include "PGShaderLib.h"
+#include "PGTexture.h"
 
 #include <memory>
 
 struct Material : DrawMaterial {
     ShaderRef shader;
+
+    PGTexture* albedoTexture;
+    PGTexture* roughnessTexture;
+    PGTexture* metallicTexture;
+    PGTexture* aoTexture;
 };
 

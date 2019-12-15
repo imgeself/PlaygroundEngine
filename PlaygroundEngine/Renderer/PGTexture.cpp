470835b998b5f97c9ed15843e4d4649bb5706571
Huseyin Karakullukcu <karakullukcuhuseyin@gmail.com>
2019-12-16 00:39:40 +0300

    DDS texture utility function implemented in PGTexture

--- PlaygroundEngine/Renderer/PGTexture.cpp
+++ PlaygroundEngine/Renderer/PGTexture.cpp
@@ -1,13 +1,22 @@
 #include "PGTexture.h"
 #include "PGRenderer.h"
 
+#include "../Utility/tinyddsloader.h"
+
 #define STB_IMAGE_IMPLEMENTATION
 #include "../Utility/stb_image.h"
 
-PGTexture::PGTexture(const std::string& filepath) {
-    int32_t width, height, comp;
+PGTexture* PGTexture::CreateTexture2D(const std::string& filepath) {
+    int32_t width, height, bytesPerPixel;
     stbi_set_flip_vertically_on_load(true);
-    void* imageData = stbi_load(filepath.c_str(), &width, &height, &comp, STBI_rgb_alpha);
+    void* imageData = stbi_load(filepath.c_str(), &width, &height, &bytesPerPixel, STBI_rgb_alpha);
+
+    // Subresources
+    TextureSubresourceData subresource;
+    subresource.data = imageData;
+    subresource.memPitch = width * 4;
+    subresource.memSlicePitch = 0;
+
 
     Texture2DInitParams initParams = {};
     initParams.arraySize = 1;
@@ -15,15 +24,61 @@ PGTexture::PGTexture(const std::string& filepath) {
     initParams.width = width;
     initParams.height = height;
     initParams.sampleCount = 1;
-    initParams.flags = TextureResourceFlags::BIND_SHADER_RESOURCE;
-    initParams.data = imageData;
+    initParams.mipCount = 1;
+    initParams.flags = TextureResourceFlags::BIND_SHADER_RESOURCE | TextureResourceFlags::USAGE_IMMUTABLE;
+    initParams.subresources = &subresource;
 
-    HWRendererAPI* rendererAPI = PGRenderer::GetRendererAPI();
-    m_HWTexture2D = rendererAPI->CreateTexture2D(&initParams);
-    m_HWShaderResourceView = rendererAPI->CreateShaderResourceView(m_HWTexture2D);
+    PGTexture* texture = new PGTexture(&initParams);
 
     stbi_image_free(imageData);
+    return texture;
 }
+
+PGTexture* PGTexture::CreateTextureFromDDSFile(const std::string& filepath) {
+    tinyddsloader::DDSFile ddsFile;
+    tinyddsloader::Result result = ddsFile.Load(filepath.c_str());
+    PG_ASSERT(result == tinyddsloader::Result::Success, "Error at loading dds file");
+
+    uint32_t arraySize = ddsFile.GetArraySize();
+    uint32_t mipCount = ddsFile.GetMipCount();
+
+    // Subresources
+    TextureSubresourceData* subresources = (TextureSubresourceData*) alloca(sizeof(TextureSubresourceData) * arraySize * mipCount);
+    for (uint32_t arrayIndex = 0; arrayIndex < arraySize; ++arrayIndex) {
+        for (uint32_t mipIndex = 0; mipIndex < mipCount; ++mipIndex) {
+            const tinyddsloader::DDSFile::ImageData* imageData = ddsFile.GetImageData(mipIndex, arrayIndex);
+            TextureSubresourceData* subresource = subresources + (arrayIndex * mipCount + mipIndex);
+            subresource->data = imageData->m_mem;
+            subresource->memPitch = imageData->m_memPitch;
+            subresource->memSlicePitch = imageData->m_memSlicePitch;
+        }
+    }
+
+
+    Texture2DInitParams initParams = {};
+    initParams.arraySize = arraySize;
+    initParams.format = (DXGI_FORMAT) ddsFile.GetFormat();
+    initParams.width = ddsFile.GetWidth();
+    initParams.height = ddsFile.GetHeight();
+    initParams.sampleCount = 1;
+    initParams.mipCount = mipCount;
+    initParams.flags = TextureResourceFlags::BIND_SHADER_RESOURCE | TextureResourceFlags::USAGE_IMMUTABLE;
+    initParams.subresources = subresources;
+
+    if (ddsFile.IsCubemap()) {
+        initParams.flags |= TextureResourceFlags::MISC_TEXTURE_CUBE;
+    }
+
+    PGTexture* texture = new PGTexture(&initParams);
+    return texture;
+}
+
+PGTexture::PGTexture(Texture2DInitParams* initParams) {
+    HWRendererAPI* rendererAPI = PGRenderer::GetRendererAPI();
+    m_HWTexture2D = rendererAPI->CreateTexture2D(initParams);
+    m_HWShaderResourceView = rendererAPI->CreateShaderResourceView(m_HWTexture2D);
+}
+
 
 PGTexture::~PGTexture() {
     delete m_HWShaderResourceView;
--- PlaygroundEngine/Renderer/PGTexture.h
+++ PlaygroundEngine/Renderer/PGTexture.h
@@ -1,20 +1,26 @@
 #pragma once
 
+#include "../Core.h"
 #include "HWRendererAPI.h"
 #include "../PGResource.h"
 
 #include <unordered_map>
 #include <string>
+#include <array>
 
 class PGTexture : public IResource {
 public:
-    PGTexture(const std::string& filepath);
     ~PGTexture();
+
+    static PGTexture* CreateTexture2D(const std::string& filepath);
+    static PGTexture* CreateTextureFromDDSFile(const std::string& filepath);
 
     inline HWTexture2D* GetHWTexture() { return m_HWTexture2D; }
     inline HWShaderResourceView* GetHWResourceView() { return m_HWShaderResourceView; }
 
 private:
+    PGTexture(Texture2DInitParams* initParams);
+
     HWTexture2D* m_HWTexture2D = nullptr;
     HWShaderResourceView* m_HWShaderResourceView = nullptr;
 

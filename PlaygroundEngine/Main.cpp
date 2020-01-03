b860a1a5d5a33265ec646e1a803008b6ed61676a
Huseyin Karakullukcu <karakullukcuhuseyin@gmail.com>
2020-01-03 21:37:52 +0300

    Enable NvOptimus

--- PlaygroundEngine/Main.cpp
+++ PlaygroundEngine/Main.cpp
@@ -3,10 +3,12 @@
 #include "PGSystem.h"
 #include "PGApplication.h"
 
+extern "C" {
+    _declspec(dllexport) DWORD NvOptimusEnablement = 0x00000001;
+}
+
 int main(int argc, char** argv)
 {
-    printf("Hello World!\n");
-
     SystemInitArguments initArguments = {};
     initArguments.executablePath = argv[0];
     initArguments.gameLibraryPath = argv[1];

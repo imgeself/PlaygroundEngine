2d432e29b21341765aabc36d0ce4eec14e9aa76b
Huseyin Karakullukcu <karakullukcuhuseyin@gmail.com>
2020-01-21 23:08:41 +0300

    Implemented base layer of render passes

---
+++ PlaygroundEngine/Renderer/BaseRenderPass.h
@@ -0,0 +1,57 @@
+#pragma once
+
+#include "HWRendererAPI.h"
+#include "PGShader.h"
+
+#define MAX_RENDER_TARGET_COUNT 4
+#define MAX_SHADER_RESOURCE_COUNT 64
+
+enum ShaderStage : uint8_t {
+    VERTEX   = BIT(1),
+    PIXEL    = BIT(2),
+    COMPUTE  = BIT(3)
+};
+
+class RenderPass {
+public:
+    virtual ~RenderPass() = default;
+
+    virtual void Execute(HWRendererAPI* rendererAPI) = 0;
+
+    inline void SetRenderTarget(size_t slot, HWRenderTargetView* renderTarget) {
+        m_RenderTargets[slot] = renderTarget;
+    }
+
+    inline void SetDepthStencilTarget(HWDepthStencilView* depthStencilTarget) {
+        m_DepthStencilView = depthStencilTarget;
+    }
+
+    inline void SetViewport(HWViewport& viewport) {
+        m_Viewport = viewport;
+    }
+
+    inline void SetShader(PGShader* shader) {
+        m_Shader = shader;
+    }
+
+    inline void SetShaderResource(size_t slot, HWShaderResourceView* shaderResource, uint8_t shaderStage) {
+        if (shaderStage & ShaderStage::VERTEX) {
+            m_ShaderResourcesVS[slot] = shaderResource;
+        }
+        if (shaderStage & ShaderStage::PIXEL) {
+            m_ShaderResourcesPS[slot] = shaderResource;
+        }
+    }
+
+protected:
+    HWDepthStencilView* m_DepthStencilView = nullptr;
+    HWRenderTargetView* m_RenderTargets[MAX_RENDER_TARGET_COUNT] = { 0 };
+    HWViewport m_Viewport;
+
+    PGShader* m_Shader = nullptr;
+
+    HWShaderResourceView* m_ShaderResourcesVS[MAX_SHADER_RESOURCE_COUNT] = { 0 };
+    HWShaderResourceView* m_ShaderResourcesPS[MAX_SHADER_RESOURCE_COUNT] = { 0 };
+
+};
+

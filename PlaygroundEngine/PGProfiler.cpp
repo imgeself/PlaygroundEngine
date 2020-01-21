3ab173bcd3e5263868c071375633b3466dfad9be
Huseyin Karakullukcu <karakullukcuhuseyin@gmail.com>
2020-01-22 00:05:21 +0300

    Allow duplicate timers

--- PlaygroundEngine/PGProfiler.cpp
+++ PlaygroundEngine/PGProfiler.cpp
@@ -19,14 +19,7 @@ void PGProfileTimer::Stop() {
     uint64_t deltaNanoSeconds = endNanoSeconds - startNanoSeconds;
 
     ProfileData profileData = { name, startNanoSeconds, deltaNanoSeconds };
-    auto search = std::find(s_ProfileResults.begin(), s_ProfileResults.end(), profileData);
-    // Update data if exists, otherwise add into the list.
-    // It is a set data structure behavior but we implemented using vector because we need to sort the list after
-    if (search != s_ProfileResults.end()) {
-        *search = profileData;
-    } else {
-        s_ProfileResults.push_back(profileData);
-    }
+    s_ProfileResults.push_back(profileData);
 
     isStopped = true;
 }

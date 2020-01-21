e7b7563634d1a53f8ad8896ceac4e370ce272373
Huseyin Karakullukcu <karakullukcuhuseyin@gmail.com>
2020-01-21 23:18:28 +0300

    Inherit SceneRenderPass from BaseRenderPass

--- PlaygroundEngine/Renderer/SceneRenderPass.cpp
+++ PlaygroundEngine/Renderer/SceneRenderPass.cpp
@@ -1,53 +1,27 @@
 #include "SceneRenderPass.h"
 #include "../PGProfiler.h"
 
-SceneRenderPass::SceneRenderPass() {
-
-}
-
-void SceneRenderPass::SetRenderTarget(size_t slot, HWRenderTargetView* renderTarget) {
-    PG_ASSERT(slot < MAX_RENDER_TARGET_COUNT, "We only support 4 render target for now");
-    m_RenderTargets[slot] = renderTarget;
-}
-
-void SceneRenderPass::SetDepthStencilTarget(HWDepthStencilView* depthStencilTarget) {
-    m_DepthStencilView = depthStencilTarget;
-}
-
-void SceneRenderPass::SetViewport(HWViewport& viewport) {
-    m_Viewport.topLeftX = viewport.topLeftX;
-    m_Viewport.topLeftY = viewport.topLeftY;
-    m_Viewport.width = viewport.width;
-    m_Viewport.height = viewport.height;
-}
-
-void SceneRenderPass::AddRenderObject(PGRenderObject* renderObject) {
-    m_RenderObjects.push_back(renderObject);
-}
-
-void SceneRenderPass::AddRenderObjects(const std::vector<PGRenderObject*>& renderObjects) {
+void SceneRenderPass::SetRenderObjects(const std::vector<PGRenderObject*>& renderObjects) {
+    m_RenderObjects.clear();
     m_RenderObjects.insert(std::end(m_RenderObjects), std::begin(renderObjects), std::end(renderObjects));
 }
 
 void SceneRenderPass::Execute(HWRendererAPI* rendererAPI) {
-    if (m_ShadowMapPass) {
-        // Generate shadow map
-        m_ShadowMapPass->Execute(rendererAPI, m_RenderObjects);
-    }
-    
     PG_PROFILE_FUNCTION();
     rendererAPI->SetRenderTargets(m_RenderTargets, MAX_RENDER_TARGET_COUNT, m_DepthStencilView);
-    float color[4] = {0.0f, 0.0f, 0.0f, 1.0f};
-    rendererAPI->ClearRenderTarget(m_RenderTargets[0], color);
+    for (size_t i = 0; i < MAX_RENDER_TARGET_COUNT; ++i) {
+        HWRenderTargetView* renderTarget = m_RenderTargets[i];
+        if (renderTarget) {
+            float color[4] = { 0.0f, 0.0f, 0.0f, 1.0f };
+            rendererAPI->ClearRenderTarget(m_RenderTargets[0], color);
+        }
+    }
     if (m_DepthStencilView) {
         rendererAPI->ClearDepthStencilView(m_DepthStencilView, false, 1.0f, 0);
     }
     rendererAPI->SetViewport(&m_Viewport);
-
-    if (m_ShadowMapPass) {
-        HWShaderResourceView* resource = m_ShadowMapPass->GetShadowMapResourceView();
-        rendererAPI->SetShaderResourcesPS(SHADOW_MAP_TEXTURE2D_SLOT, &resource, 1);
-    }
+    rendererAPI->SetVertexShader(m_Shader->GetHWVertexShader());
+    rendererAPI->SetPixelShader(m_Shader->GetHWPixelShader());
 
     for (PGRenderObject* renderObject : m_RenderObjects) {
         renderObject->UpdatePerDrawConstantBuffer(rendererAPI);
@@ -68,14 +42,15 @@ void SceneRenderPass::Execute(HWRendererAPI* rendererAPI) {
             irradianceMap ? irradianceMap->GetHWResourceView() : nullptr,
             envBrdf ? envBrdf->GetHWResourceView() : nullptr,
         };
-        rendererAPI->SetShaderResourcesPS(ALBEDO_TEXTURE2D_SLOT, textureResources, ARRAYSIZE(textureResources));
+        memcpy(m_ShaderResourcesPS + ALBEDO_TEXTURE2D_SLOT, textureResources, sizeof(textureResources));
+        
+        rendererAPI->SetShaderResourcesVS(0, m_ShaderResourcesVS, MAX_SHADER_RESOURCE_COUNT);
+        rendererAPI->SetShaderResourcesPS(0, m_ShaderResourcesPS, MAX_SHADER_RESOURCE_COUNT);
 
         size_t vertexBufferStride = sizeof(Vertex);
         rendererAPI->SetVertexBuffer(renderObject->vertexBuffer, vertexBufferStride);
         rendererAPI->SetIndexBuffer(renderObject->indexBuffer);
         rendererAPI->SetInputLayout(renderObject->inputLayout);
-        rendererAPI->SetVertexShader(renderObject->shader->GetHWVertexShader());
-        rendererAPI->SetPixelShader(renderObject->shader->GetHWPixelShader());
         if (renderObject->indexBuffer) {
             rendererAPI->DrawIndexed(renderObject->indexBuffer);
         } else {
--- PlaygroundEngine/Renderer/SceneRenderPass.h
+++ PlaygroundEngine/Renderer/SceneRenderPass.h
@@ -2,34 +2,16 @@
 
 #include "HWRendererAPI.h"
 #include "PGRenderObject.h"
-#include "ShadowMapPass.h"
 
-#define MAX_RENDER_TARGET_COUNT 4
+#include "BaseRenderPass.h"
 
-class SceneRenderPass {
+class SceneRenderPass : public RenderPass {
 public:
-    SceneRenderPass();
+    virtual ~SceneRenderPass() = default;
 
-    void SetRenderTarget(size_t slot, HWRenderTargetView* renderTarget);
-    void SetDepthStencilTarget(HWDepthStencilView* depthStencilTarget);
-    void SetViewport(HWViewport& viewport);
+    void SetRenderObjects(const std::vector<PGRenderObject*>& renderObjects);
 
-    void AddRenderObject(PGRenderObject* renderObject);
-    void AddRenderObjects(const std::vector<PGRenderObject*>& renderObjects);
-
-    void Execute(HWRendererAPI* rendererAPI);
-
-    inline void SetShadowMapPass(ShadowMapPass* shadowMapPass) { 
-        m_ShadowMapPass = shadowMapPass;
-    }
-
-private:
-    HWDepthStencilView* m_DepthStencilView = nullptr;
-    HWRenderTargetView* m_RenderTargets[MAX_RENDER_TARGET_COUNT] = {0};
-    size_t m_RenderTargetCount;
-    HWViewport m_Viewport;
-
-    ShadowMapPass* m_ShadowMapPass;
-
+    virtual void Execute(HWRendererAPI* rendererAPI) override;
+protected:
     std::vector<PGRenderObject*> m_RenderObjects;
 };

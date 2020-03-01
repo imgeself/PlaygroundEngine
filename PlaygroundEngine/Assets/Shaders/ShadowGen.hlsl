7ec904adf6cc34901b4e0fd8546874a887449699
Huseyin Karakullukcu <karakullukcuhuseyin@gmail.com>
2020-03-01 23:30:19 +0300

    Position only vertex input in ShadowGen shader

--- PlaygroundEngine/Assets/Shaders/ShadowGen.hlsl
+++ PlaygroundEngine/Assets/Shaders/ShadowGen.hlsl
@@ -7,7 +7,7 @@ struct VSOut {
     float4 pos : SV_Position;
     uint depthSlice : SV_RenderTargetArrayIndex;
 };
-VSOut VSMain(float3 pos : POSITION, float3 normal : NORMAL, float2 texCoord : TEXCOORD) {
+VSOut VSMain(float3 pos : POSITION) {
     VSOut output;
     output.pos = mul(g_LightProjMatrices[g_ShadowGenCascadeIndex], mul(g_LightViewMatrix, mul(g_ModelMatrix, float4(pos, 1.0f))));
     output.depthSlice = g_ShadowGenCascadeIndex;

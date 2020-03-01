c239744ee0ee56a835fb0f632ee6a2122316afa1
Huseyin Karakullukcu <karakullukcuhuseyin@gmail.com>
2020-03-01 23:27:38 +0300

    Used RenderScene function in ShadowMapStage
    Used RenderScene function directly in ShadowMapStage instead of SceneRenderPass

--- PlaygroundEngine/Renderer/ShadowMapStage.cpp
+++ PlaygroundEngine/Renderer/ShadowMapStage.cpp
@@ -1,18 +1,13 @@
 #include "ShadowMapStage.h"
 #include "../PGProfiler.h"
 
-void ShadowGenStage::Initialize(HWRendererAPI* rendererAPI, PGShaderLib* shaderLib, size_t shadowMapSize) {
-    HWViewport shadowViewport;
-    shadowViewport.topLeftX = 0.0f;
-    shadowViewport.topLeftY = 0.0f;
-    shadowViewport.width = (float) shadowMapSize;
-    shadowViewport.height = (float) shadowMapSize;
+void ShadowGenStage::Initialize(HWRendererAPI* rendererAPI, GPUResource* shadowMapTarget, size_t shadowMapSize) {
+    m_ShadowMapViewport.topLeftX = 0.0f;
+    m_ShadowMapViewport.topLeftY = 0.0f;
+    m_ShadowMapViewport.width = (float) shadowMapSize;
+    m_ShadowMapViewport.height = (float) shadowMapSize;
 
-    PGShader* shadowGenShader = shaderLib->GetDefaultShader("ShadowGen");
-    
-    // Init pass
-    m_ShadowGenPass.SetShader(shadowGenShader);
-    m_ShadowGenPass.SetViewport(shadowViewport);
+    m_ShadowMapTarget = shadowMapTarget->dsv;
 
     PerShadowGenConstantBuffer initData = {};
     m_PerShadowGenConstantBuffer = rendererAPI->CreateConstantBuffer(&initData, sizeof(PerShadowGenConstantBuffer), 
@@ -20,14 +15,16 @@ void ShadowGenStage::Initialize(HWRendererAPI* rendererAPI, PGShaderLib* shaderL
     rendererAPI->SetConstanBuffersVS(PER_SHADOWGEN_CBUFFER_SLOT, &m_PerShadowGenConstantBuffer, 1);
 }
 
-void ShadowGenStage::Execute(HWRendererAPI* rendererAPI, const std::vector<PGRenderObject*>& shadowCasters) {
+void ShadowGenStage::Execute(HWRendererAPI* rendererAPI, const RenderList& shadowCasterList, PGShaderLib* shaderLib) {
     PG_PROFILE_FUNCTION();
-    m_ShadowGenPass.SetRenderObjects(shadowCasters);
-    rendererAPI->SetRenderTargets(nullptr, 0, m_DepthStencilView);
-    rendererAPI->ClearDepthStencilView(m_DepthStencilView, false, 1.0f, 0);
-    rendererAPI->SetViewport(&m_ShadowGenPass.m_Viewport);
-    rendererAPI->SetVertexShader(m_ShadowGenPass.m_Shader->GetHWVertexShader());
+    rendererAPI->SetRenderTargets(nullptr, 0, m_ShadowMapTarget);
+    rendererAPI->ClearDepthStencilView(m_ShadowMapTarget, false, 1.0f, 0);
+    rendererAPI->SetViewport(&m_ShadowMapViewport);
+
+    PGShader* shadowGenShader = shaderLib->GetDefaultShader("ShadowGen");
+    rendererAPI->SetVertexShader(shadowGenShader->GetHWVertexShader());
     rendererAPI->SetPixelShader(nullptr); // We don't need a pixel shader in shadow generation stage
+
     for (uint32_t cascadeIndex = 0; cascadeIndex < CASCADE_COUNT; ++cascadeIndex) {
         void* gpuBuffer = rendererAPI->Map(m_PerShadowGenConstantBuffer);
         PerShadowGenConstantBuffer data = {};
@@ -35,7 +32,7 @@ void ShadowGenStage::Execute(HWRendererAPI* rendererAPI, const std::vector<PGRen
         memcpy(gpuBuffer, &data, sizeof(PerShadowGenConstantBuffer));
         rendererAPI->Unmap(m_PerShadowGenConstantBuffer);
 
-        m_ShadowGenPass.Execute(rendererAPI);
+        RenderScene(rendererAPI, shadowCasterList, SceneRenderPassType::SHADOW_GEN);
     }
 }
 
--- PlaygroundEngine/Renderer/ShadowMapStage.h
+++ PlaygroundEngine/Renderer/ShadowMapStage.h
@@ -7,35 +7,13 @@ const uint32_t CASCADE_COUNT = 4;
 
 class ShadowGenStage {
 public:
-    void Initialize(HWRendererAPI* rendererAPI, PGShaderLib* shaderLib, size_t shadowMapSize);
-    void Execute(HWRendererAPI* rendererAPI, const std::vector<PGRenderObject*>& shadowCasters);
-
-    inline void SetShadowMapDSV(HWDepthStencilView* dsv) { m_DepthStencilView = dsv; }
+    void Initialize(HWRendererAPI* rendererAPI, GPUResource* shadowMapTarget, size_t shadowMapSize);
+    void Execute(HWRendererAPI* rendererAPI, const RenderList& shadowCasterList, PGShaderLib* shaderLib);
 
 private:
-    class ShadowGenPass : public SceneRenderPass {
-        friend class ShadowGenStage;
-    public:
-        virtual ~ShadowGenPass() = default;
-
-        virtual void Execute(HWRendererAPI* rendererAPI) {
-            PG_PROFILE_FUNCTION();
-            for (PGRenderObject* renderObject : m_RenderObjects) {
-                renderObject->UpdatePerDrawConstantBuffer(rendererAPI);
-
-                uint32_t vertexBufferStride = sizeof(Vertex);
-                uint32_t offset = 0;
-                rendererAPI->SetVertexBuffers(&renderObject->vertexBuffer, 1, &vertexBufferStride, &offset);
-                rendererAPI->SetIndexBuffer(renderObject->indexBuffer);
-                rendererAPI->SetInputLayout(renderObject->inputLayout);
-                rendererAPI->DrawIndexed(renderObject->indexBuffer);
-            }
-        }
-    };
-
-    HWDepthStencilView* m_DepthStencilView;
     HWConstantBuffer* m_PerShadowGenConstantBuffer;
 
-    ShadowGenStage::ShadowGenPass m_ShadowGenPass;
+    HWDepthStencilView* m_ShadowMapTarget;
+    HWViewport m_ShadowMapViewport;
 };
 

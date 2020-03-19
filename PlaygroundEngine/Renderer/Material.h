9e783642d3a95cb569551cc98d286a9db34a275f
Huseyin Karakullukcu <karakullukcuhuseyin@gmail.com>
2020-03-19 17:18:21 +0300

    Normal map texture added in Material struct

--- PlaygroundEngine/Renderer/Material.h
+++ PlaygroundEngine/Renderer/Material.h
@@ -17,6 +17,7 @@ struct Material : DrawMaterial {
     PGTexture* metallicTexture;
     PGTexture* aoTexture;
     PGTexture* metallicRoughnessTexture;
+    PGTexture* normalTexture;
 
     PGTexture* radianceMap;
     PGTexture* irradianceMap;
@@ -26,6 +27,7 @@ struct Material : DrawMaterial {
     ShaderRef shader; // TODO: Store technique instead of shader itself?
     AlphaMode alphaMode;
     bool doubleSided;
+    bool normalMappingEnabled;
 
     uint16_t GetMaterialHash() {
         size_t pipelineOffset = offsetof(Material, shader);
@@ -37,15 +39,16 @@ struct Material : DrawMaterial {
     uint16_t GetPipelineHash() {
         union {
             struct {
-                uint16_t shader      : 13;
-                uint16_t doubleSided : 1;
-                uint16_t alphaMode   : 2;
+                uint16_t shader        : 12;
+                uint16_t normalMapping : 1;
+                uint16_t doubleSided   : 1;
+                uint16_t alphaMode     : 2;
             };
 
             uint16_t hash;
         } pipelineKey;
 
-
+        pipelineKey.normalMapping = normalMappingEnabled;
         pipelineKey.shader = ((size_t) shader) & 0xFFFF;
         pipelineKey.doubleSided = doubleSided;
         pipelineKey.alphaMode = (uint16_t) alphaMode;

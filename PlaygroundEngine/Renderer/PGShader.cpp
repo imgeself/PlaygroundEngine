25b156c6524af36a348f9ce231288ad6098413b9
Huseyin Karakullukcu <karakullukcuhuseyin@gmail.com>
2020-03-19 17:30:23 +0300

    Shader permutation support

--- PlaygroundEngine/Renderer/PGShader.cpp
+++ PlaygroundEngine/Renderer/PGShader.cpp
@@ -6,11 +6,30 @@ PGShader::PGShader() {
 }
 
 PGShader::~PGShader() {
-    SAFE_RELEASE(m_VertexShaderBlob);
-    SAFE_RELEASE(m_PixelShaderBlob);
+    for (PGShaderPermutation& permutation : m_Permutations) {
+        SAFE_RELEASE(permutation.vertexShaderBlob.shaderBlob);
+        SAFE_RELEASE(permutation.pixelShaderBlob.shaderBlob);
+    }
 }
 
-static bool CompileShader(const char* filename, const char* mainFunctionName, const char* version, ID3DBlob** shaderBlob) {
+static void ApplyShaderFlags(D3D_SHADER_MACRO* compileDefines, uint32_t shaderFlags) {
+    compileDefines[0].Name = "_HLSL";
+    compileDefines[0].Definition = "1";
+
+    size_t compileDefineIndex = 1;
+    if (shaderFlags & PGShaderFlags::ALPHA_TEST) {
+        compileDefines[compileDefineIndex].Name = "ALPHA_TEST";
+        compileDefines[compileDefineIndex].Definition = "1";
+        ++compileDefineIndex;
+    }
+    if (shaderFlags & PGShaderFlags::NORMAL_MAPPING) {
+        compileDefines[compileDefineIndex].Name = "NORMAL_MAPPING";
+        compileDefines[compileDefineIndex].Definition = "1";
+        ++compileDefineIndex;
+    }
+}
+
+static bool CompileShader(uint32_t shaderFlags, const char* filename, const char* mainFunctionName, const char* version, ID3DBlob** shaderBlob) {
     ID3DBlob* errorBlob = nullptr;
 
     const size_t filenameSize = strlen(filename) + 1;
@@ -19,7 +38,8 @@ static bool CompileShader(const char* filename, const char* mainFunctionName, co
     size_t numberOfConverted;
     mbstowcs_s(&numberOfConverted, wcFilename, filenameSize, filename, filenameSize - 1);
 
-    D3D_SHADER_MACRO compileDefines[] = { "_HLSL", "1", NULL, NULL };
+    D3D_SHADER_MACRO compileDefines[16] = {0};
+    ApplyShaderFlags(compileDefines, shaderFlags);
     HRESULT status = D3DCompileFromFile(wcFilename, compileDefines, D3D_COMPILE_STANDARD_FILE_INCLUDE,
         mainFunctionName, version, D3DCOMPILE_DEBUG, 0, shaderBlob, &errorBlob);
     if (errorBlob) {
@@ -30,26 +50,70 @@ static bool CompileShader(const char* filename, const char* mainFunctionName, co
     return SUCCEEDED(status);
 }
 
-HWShaderBytecode PGShader::GetVertexBytecode() {
-    if (!m_VertexShaderBlob) {
-        PG_LOG_DEBUG("Compiling vertex shader: %s", m_Filepath.c_str());
-        bool vertexCompilationSuccess = CompileShader(m_Filepath.c_str(), "VSMain", "vs_5_0", &m_VertexShaderBlob);
-        PG_ASSERT(vertexCompilationSuccess, "Shader compilation failed!")
+HWShaderBytecode PGShader::GetVertexBytecode(uint32_t shaderFlags) {
+    auto searchIterator = std::find_if(m_Permutations.begin(), m_Permutations.end(), [shaderFlags](PGShaderPermutation& permutation) {
+        return permutation.shaderFlags == shaderFlags;
+    });
+
+    ID3DBlob* shaderBlob = nullptr;
+    if (searchIterator != m_Permutations.end()) {
+        if (searchIterator->vertexShaderBlob.isDirty) {
+            PG_LOG_DEBUG("Compiling vertex shader: %s with flags: %d", m_Filepath.c_str(), shaderFlags);
+            bool vertexCompilationSuccess = CompileShader(shaderFlags, m_Filepath.c_str(), "VSMain", "vs_5_0", &shaderBlob);
+            PG_ASSERT(vertexCompilationSuccess, "Shader compilation failed!");
+            searchIterator->vertexShaderBlob.shaderBlob = shaderBlob;
+            searchIterator->vertexShaderBlob.isDirty = false;
+        }
+    } else {
+        PG_LOG_DEBUG("Compiling vertex shader: %s with flags: %d", m_Filepath.c_str(), shaderFlags);
+        bool vertexCompilationSuccess = CompileShader(shaderFlags, m_Filepath.c_str(), "VSMain", "vs_5_0", &shaderBlob);
+        PG_ASSERT(vertexCompilationSuccess, "Shader compilation failed!");
+
+        PGShaderPermutation shaderPermutation = {};
+        shaderPermutation.vertexShaderBlob.shaderBlob = shaderBlob;
+        shaderPermutation.vertexShaderBlob.isDirty = false;
+        shaderPermutation.shaderFlags = shaderFlags;
+
+        m_Permutations.push_back(shaderPermutation);
     }
 
-    HWShaderBytecode bytecode = { m_VertexShaderBlob->GetBufferPointer(), m_VertexShaderBlob->GetBufferSize() };
+    
+
+    HWShaderBytecode bytecode = { shaderBlob->GetBufferPointer(), shaderBlob->GetBufferSize() };
     return bytecode;
 }
 
-HWShaderBytecode PGShader::GetPixelBytecode() {
-    if (!m_PixelShaderBlob) {
-        PG_LOG_DEBUG("Compiling pixel shader: %s", m_Filepath.c_str());
-        ID3DBlob* pixelShaderBlob = nullptr;
-        bool pixelCompilationSuccess = CompileShader(m_Filepath.c_str(), "PSMain", "ps_5_0", &m_PixelShaderBlob);
-        PG_ASSERT(pixelCompilationSuccess, "Shader compilation failed!")
+HWShaderBytecode PGShader::GetPixelBytecode(uint32_t shaderFlags) {
+    auto searchIterator = std::find_if(m_Permutations.begin(), m_Permutations.end(), [shaderFlags](PGShaderPermutation& permutation) {
+        return permutation.shaderFlags == shaderFlags;
+    });
+
+    ID3DBlob* shaderBlob = nullptr;
+    if (searchIterator != m_Permutations.end()) {
+        if (searchIterator->pixelShaderBlob.isDirty) {
+            PG_LOG_DEBUG("Compiling pixel shader: %s with flags: %d", m_Filepath.c_str(), shaderFlags);
+            bool pixelCompilationSuccess = CompileShader(shaderFlags, m_Filepath.c_str(), "PSMain", "ps_5_0", &shaderBlob);
+            PG_ASSERT(pixelCompilationSuccess, "Shader compilation failed!");
+            searchIterator->pixelShaderBlob.shaderBlob = shaderBlob;
+            searchIterator->pixelShaderBlob.isDirty = false;
+        }
+    }
+    else {
+        PG_LOG_DEBUG("Compiling pixel shader: %s with flags: %d", m_Filepath.c_str(), shaderFlags);
+        bool pixelCompilationSuccess = CompileShader(shaderFlags, m_Filepath.c_str(), "PSMain", "ps_5_0", &shaderBlob);
+        PG_ASSERT(pixelCompilationSuccess, "Shader compilation failed!");
+
+        PGShaderPermutation shaderPermutation = {};
+        shaderPermutation.pixelShaderBlob.shaderBlob = shaderBlob;
+        shaderPermutation.pixelShaderBlob.isDirty = false;
+        shaderPermutation.shaderFlags = shaderFlags;
+
+        m_Permutations.push_back(shaderPermutation);
     }
 
-    HWShaderBytecode bytecode = { m_PixelShaderBlob->GetBufferPointer(), m_PixelShaderBlob->GetBufferSize() };
+
+
+    HWShaderBytecode bytecode = { shaderBlob->GetBufferPointer(), shaderBlob->GetBufferSize() };
     return bytecode;
 }
 
@@ -60,7 +124,26 @@ void PGShader::LoadFromFilename(const std::string& filename) {
 void PGShader::Reload() {
     if (needsUpdate) {
         needsUpdate = false;
-        SAFE_RELEASE(m_VertexShaderBlob);
-        SAFE_RELEASE(m_PixelShaderBlob);
+
+        PG_LOG_DEBUG("Reloading shader: %s", m_Filepath.c_str());
+        ID3DBlob* vertexShaderBlob = nullptr;
+        bool vertexCompilationSuccess = CompileShader(PGShaderFlags::ALL_FLAGS, m_Filepath.c_str(), "VSMain", "vs_5_0", &vertexShaderBlob);
+        ID3DBlob* pixelShaderBlob = nullptr;
+        bool pixelCompilationSuccess = CompileShader(PGShaderFlags::ALL_FLAGS, m_Filepath.c_str(), "PSMain", "ps_5_0", &pixelShaderBlob);
+
+        if (!vertexCompilationSuccess || !pixelCompilationSuccess) {
+            PG_LOG_ERROR("Shader reload failed!");
+            return;
+        }
+
+        SAFE_RELEASE(vertexShaderBlob);
+        SAFE_RELEASE(pixelShaderBlob);
+
+        for (PGShaderPermutation& permutation : m_Permutations) {
+            permutation.vertexShaderBlob.isDirty = true;
+            permutation.pixelShaderBlob.isDirty = true;
+        }
+
+        PG_LOG_DEBUG("Shader reloaded sucessfully");
     }
 }
--- PlaygroundEngine/Renderer/PGShader.h
+++ PlaygroundEngine/Renderer/PGShader.h
@@ -8,13 +8,31 @@
 
 #include <d3dcompiler.h>
 
+enum PGShaderFlags : uint32_t {
+    ALPHA_TEST     = BIT(1),
+    NORMAL_MAPPING = BIT(2),
+
+    ALL_FLAGS = ALPHA_TEST | NORMAL_MAPPING
+};
+
+struct PGShaderBlob {
+    ID3DBlob* shaderBlob = nullptr;
+    bool isDirty = true;
+};
+
+struct PGShaderPermutation {
+    PGShaderBlob vertexShaderBlob;
+    PGShaderBlob pixelShaderBlob;
+    uint32_t shaderFlags = 0;
+};
+
 class PGShader {
 public:
     PGShader();
     ~PGShader();
 
-    HWShaderBytecode GetVertexBytecode();
-    HWShaderBytecode GetPixelBytecode();
+    HWShaderBytecode GetVertexBytecode(uint32_t shaderFlags = 0);
+    HWShaderBytecode GetPixelBytecode(uint32_t shaderFlags = 0);
 
     void LoadFromFilename(const std::string& filename);
     void Reload();
@@ -23,7 +41,6 @@ public:
 
 private:
     std::string m_Filepath;
-    ID3DBlob* m_VertexShaderBlob = nullptr;
-    ID3DBlob* m_PixelShaderBlob = nullptr;
+    std::vector<PGShaderPermutation> m_Permutations;
 };
 

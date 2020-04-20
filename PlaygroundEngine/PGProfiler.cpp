8c4665dfbef0c35b685d7839db78a44dae2b2287
Huseyin Karakullukcu <karakullukcuhuseyin@gmail.com>
2020-04-21 00:49:45 +0300

    GPU profiling utilities

--- PlaygroundEngine/PGProfiler.cpp
+++ PlaygroundEngine/PGProfiler.cpp
@@ -25,4 +25,145 @@ void PGProfileTimer::Stop() {
 }
 
 
+std::unordered_map<std::string, size_t> PGGPUProfilerTimer::s_ProfileEntries;
+
+bool PGGPUProfilerTimer::s_FirstPass = true;
+size_t PGGPUProfilerTimer::s_CurrentFrameIndex = 0;
+size_t PGGPUProfilerTimer::s_FreeQueryIndex = 1; // 0 is reserved for frame timer
+
+HWQuery* PGGPUProfilerTimer::s_TimestampDisjointQueries[GPU_PROFILER_FRAME_LATENCY] = {0};
+
+HWQuery* PGGPUProfilerTimer::s_StartQueries[GPU_PROFILER_MAX_QUERY_COUNT][GPU_PROFILER_FRAME_LATENCY] = {0};
+HWQuery* PGGPUProfilerTimer::s_EndQueries[GPU_PROFILER_MAX_QUERY_COUNT][GPU_PROFILER_FRAME_LATENCY] = {0};
+
+PGGPUProfilerTimer::PGGPUProfilerTimer(HWRendererAPI* rendererAPI, const char* name)
+    : name(name), isStopped(false), rendererAPI(rendererAPI) {
+
+    size_t queryIndex;
+    auto searchEntry = s_ProfileEntries.find(name);
+    if (searchEntry != s_ProfileEntries.end()) {
+        queryIndex = searchEntry->second;
+    } else {
+        queryIndex = s_FreeQueryIndex++;
+        s_ProfileEntries[name] = queryIndex;
+    }
+
+    rendererAPI->EndQuery(s_StartQueries[queryIndex][s_CurrentFrameIndex]);
+}
+
+PGGPUProfilerTimer::~PGGPUProfilerTimer() {
+    if (!isStopped) {
+        Stop(rendererAPI, name);
+    }
+}
+
+void PGGPUProfilerTimer::Stop(HWRendererAPI* rendererAPI, const char* name) {
+    auto searchEntry = s_ProfileEntries.find(name);
+    if (searchEntry != s_ProfileEntries.end()) {
+        size_t queryIndex = searchEntry->second;
+        rendererAPI->EndQuery(s_EndQueries[queryIndex][s_CurrentFrameIndex]);
+    }
+    else {
+        PG_ASSERT(false, "Can't find a GPU profile entry with this name");
+    }
+
+    isStopped = true;
+}
+
+void PGGPUProfilerTimer::Initialize(HWRendererAPI* rendererAPI) {
+    for (size_t i = 0; i < GPU_PROFILER_FRAME_LATENCY; i++) {
+        HWQueryDesc queryDesc;
+        queryDesc.queryType = QUERY_TIMESTAMP_DISJOINT;
+        queryDesc.flags = 0;
+
+        s_TimestampDisjointQueries[i] = rendererAPI->CreateQuery(queryDesc);
+    }
+
+    for (size_t i = 0; i < GPU_PROFILER_MAX_QUERY_COUNT; i++) {
+        for (size_t j = 0; j < GPU_PROFILER_FRAME_LATENCY; j++) {
+            HWQueryDesc queryDesc;
+            queryDesc.queryType = QUERY_TIMESTAMP;
+            queryDesc.flags = 0;
+
+            s_StartQueries[i][j] = rendererAPI->CreateQuery(queryDesc);
+            s_EndQueries[i][j] = rendererAPI->CreateQuery(queryDesc);
+        }
+    }
+}
+
+void PGGPUProfilerTimer::Destroy() {
+    for (size_t i = 0; i < GPU_PROFILER_FRAME_LATENCY; i++) {
+        delete s_TimestampDisjointQueries[i];
+    }
+
+    for (size_t i = 0; i < GPU_PROFILER_MAX_QUERY_COUNT; i++) {
+        for (size_t j = 0; j < GPU_PROFILER_FRAME_LATENCY; j++) {
+            delete s_StartQueries[i][j];
+            delete s_EndQueries[i][j];
+        }
+    }
+}
+
+void PGGPUProfilerTimer::BeginFrame(HWRendererAPI* rendererAPI) {
+    rendererAPI->BeginQuery(s_TimestampDisjointQueries[s_CurrentFrameIndex]);
+    rendererAPI->EndQuery(s_StartQueries[0][s_CurrentFrameIndex]);
+}
+
+void PGGPUProfilerTimer::EndFrame(HWRendererAPI* rendererAPI) {
+    rendererAPI->EndQuery(s_EndQueries[0][s_CurrentFrameIndex]);
+    rendererAPI->EndQuery(s_TimestampDisjointQueries[s_CurrentFrameIndex]);
+    if (s_FirstPass && s_CurrentFrameIndex == (GPU_PROFILER_FRAME_LATENCY - 1)) {
+        s_FirstPass = false;
+    }
+    s_CurrentFrameIndex = ++s_CurrentFrameIndex % GPU_PROFILER_FRAME_LATENCY;
+}
+
+void PGGPUProfilerTimer::GetProfileList(HWRendererAPI* rendererAPI, ProfileList& outProfileList) {
+    if (s_FirstPass) {
+        return;
+    }
+
+    size_t fetchFrameIndex = s_CurrentFrameIndex;
+    D3D11_QUERY_DATA_TIMESTAMP_DISJOINT disjointData;
+    while (!rendererAPI->GetDataQuery(s_TimestampDisjointQueries[fetchFrameIndex], &disjointData, sizeof(D3D11_QUERY_DATA_TIMESTAMP_DISJOINT), 0)) {}
+
+    if (disjointData.Disjoint) {
+        return;
+    }
+
+    // Frame timer
+    {
+        uint64_t tsStart, tsEnd;
+        rendererAPI->GetDataQuery(s_StartQueries[0][fetchFrameIndex], &tsStart, sizeof(uint64_t), 0);
+        rendererAPI->GetDataQuery(s_EndQueries[0][fetchFrameIndex], &tsEnd, sizeof(uint64_t), 0);
+
+        uint64_t startNanoSeconds = (tsStart * 1000000000) / disjointData.Frequency;
+        uint64_t durationNanoSeconds = ((tsEnd - tsStart) * 1000000000) / disjointData.Frequency;
+
+        ProfileData profileData;
+        profileData.name = "GPU Frame";
+        profileData.startNanoSeconds = startNanoSeconds;
+        profileData.durationNanoSeconds = durationNanoSeconds;
+
+        outProfileList.push_back(profileData);
+    }
+
+    for (auto& entry : s_ProfileEntries) {
+        size_t queryIndex = entry.second;
+        uint64_t tsStart, tsEnd;
+        rendererAPI->GetDataQuery(s_StartQueries[queryIndex][fetchFrameIndex], &tsStart, sizeof(uint64_t), 0);
+        rendererAPI->GetDataQuery(s_EndQueries[queryIndex][fetchFrameIndex], &tsEnd, sizeof(uint64_t), 0);
+
+        uint64_t startNanoSeconds = (tsStart * 1000000000) / disjointData.Frequency;
+        uint64_t durationNanoSeconds = ((tsEnd - tsStart) * 1000000000) / disjointData.Frequency;
+
+        ProfileData profileData;
+        profileData.name = entry.first;
+        profileData.startNanoSeconds = startNanoSeconds;
+        profileData.durationNanoSeconds = durationNanoSeconds;
+
+        outProfileList.push_back(profileData);
+    }
+}
+
 
--- PlaygroundEngine/PGProfiler.h
+++ PlaygroundEngine/PGProfiler.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "Core.h"
+#include "Renderer/HWRendererAPI.h"
 
 #include <string>
 #include <vector>
@@ -31,7 +32,53 @@ public:
     static ProfileList s_ProfileResults;
 };
 
-#define PG_PROFILE_SCOPE(name) PGProfileTimer timer##__LINE__(name);
+#define PG_PROFILE(name, line) PGProfileTimer timer##line(name);
+// Need some extra layer of indirection for concatenating a line number instead of "__LINE__" string
+// https://stackoverflow.com/a/1597129
+#define PG_PROFILE2(name, line) PG_PROFILE(name, line)
+#define PG_PROFILE_SCOPE(name) PG_PROFILE2(name, __LINE__)
 #define PG_PROFILE_FUNCTION() PG_PROFILE_SCOPE(__FUNCTION__)
 
+#define GPU_PROFILER_FRAME_LATENCY 4
+#define GPU_PROFILER_MAX_QUERY_COUNT 20
+
+class PG_API PGGPUProfilerTimer {
+public:
+    PGGPUProfilerTimer(HWRendererAPI* rendererAPI, const char* name);
+    ~PGGPUProfilerTimer();
+    void Stop(HWRendererAPI* rendererAPI, const char* name);
+
+    // public members
+    const char* name;
+    HWRendererAPI* rendererAPI;
+    bool isStopped;
+
+// Static members
+public:
+    static void BeginFrame(HWRendererAPI* rendererAPI);
+    static void EndFrame(HWRendererAPI* rendererAPI);
+
+    static void Initialize(HWRendererAPI* rendererAPI);
+    static void Destroy();
+
+    static void GetProfileList(HWRendererAPI* rendererAPI, ProfileList& outProfileList);
+
+private:
+    static std::unordered_map<std::string, size_t> s_ProfileEntries;
+
+    static bool s_FirstPass;
+    static size_t s_CurrentFrameIndex;
+    static size_t s_FreeQueryIndex;
+    static HWQuery* s_TimestampDisjointQueries[GPU_PROFILER_FRAME_LATENCY];
+
+    static HWQuery* s_StartQueries[GPU_PROFILER_MAX_QUERY_COUNT][GPU_PROFILER_FRAME_LATENCY];
+    static HWQuery* s_EndQueries[GPU_PROFILER_MAX_QUERY_COUNT][GPU_PROFILER_FRAME_LATENCY];
+};
+
+#define PG_GPU_PROFILE(pRendererAPI, name, line) PGGPUProfilerTimer gputimer##line(pRendererAPI, name);
+// Need some extra layer of indirection for concatenating a line number instead of "__LINE__" string
+// https://stackoverflow.com/a/1597129
+#define PG_GPU_PROFILE2(pRendererAPI, name, line) PG_GPU_PROFILE(pRendererAPI, name, line)
+#define PG_GPU_PROFILE_SCOPE(pRendererAPI, name) PG_GPU_PROFILE2(pRendererAPI, name, __LINE__)
+#define PG_GPU_PROFILE_FUNCTION(pRendererAPI) PG_GPU_PROFILE_SCOPE(pRendererAPI, __FUNCTION__)
 
